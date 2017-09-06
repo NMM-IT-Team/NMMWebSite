835fe5cad700e87aa3f538391585094439d2ace1
rsonam <rathoresonam@gmail.com>
2017-09-06 16:34:56 -0500

    changes to comments.html and php

--- public_html/comments.php
+++ public_html/comments.php
@@ -1,8 +1,9 @@
 <?php 
-$Name= $_POST["username"];
-$Email= $_POST["email"];
-$Mobile=$_POST["mobile"];
-$Comments= $_POST["comment"];
-echo "Thank you!!!.We have recieved your feedback. It would be published after the review soon!!!";
+$Name = $_POST['username'];
+$Email = $_POST['email'];
+$Mobile = $_POST['mobile'];
+$Comments = $_POST['comment'];
+echo "Thank you!!!We have recieved your feedback. It would be published after the review soon!!!" ;
 ?>
+
 
