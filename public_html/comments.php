7c98b0a89407cbfac5ae0f30dae2d5f1416f0c34
rsonam <rathoresonam@gmail.com>
2017-09-06 15:59:01 -0500

    made changes to comments.html and created comments.php page

---
+++ public_html/comments.php
@@ -0,0 +1,8 @@
+<?php 
+$Name= $_POST["username"];
+$Email= $_POST["email"];
+$Mobile=$_POST["mobile"];
+$Comments= $_POST["comment"];
+echo "Thank you!!!.We have recieved your feedback. It would be published after the review soon!!!";
+?>
+
