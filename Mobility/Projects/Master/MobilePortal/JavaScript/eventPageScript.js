aaae54f11cb2719265aaff083a76c3b19163da14
Ravi R Dixit <ravirdixit@Ravis-MacBook-Pro.local>
2018-01-28 11:05:30 -0600

    Basic validations to UI elements

--- Mobility/Projects/Master/MobilePortal/JavaScript/eventPageScript.js
+++ Mobility/Projects/Master/MobilePortal/JavaScript/eventPageScript.js
@@ -1,6 +1,8 @@
+"use strict";
+
 var app = angular.module("eventApp", []);
 
-app.controller("eventAppController", function ($scope) {
+app.controller("eventAppController", ['$scope', function ($scope) {
 
     //FIXME
     //Hack to avoid the HTML input date type to select todays date dynamically
@@ -23,18 +25,36 @@ app.controller("eventAppController", function ($scope) {
         Contact: "",
         PhoneNo: "",
         Description: ""
-    }
+    };
 
-    /*TODO: add validation to make sure the submit button is enabled only when all fields are added*/
-    $scope.ValidateEventModel = function () {
+    $scope.ResetEventModel = function () {
+        $scope.Event = {};
+    };
 
-    }
+    $scope.IsValidData = function () {
+        //TODO: validation using regex for names and other field value
+    };
 
     $scope.SaveEvent = function () {
-
-    }
+        //TODO: Validate the scope model and call the service
+        if ($scope.EventForm.$valid) {
+            //TODO: Call service as all fields are filled
+            alert('TODO: Call service and save data');
+        } else {
+            alert('Please enter all the details of the event before you submit');
+        }
+    };
 
     $scope.ResetEventPage = function () {
 
-    }
-});
+        //TODO:Only if the element has changed you should show the pop up
+        if (confirm("Would you like to reset the form?")) {
+            $('#EventInputForm input[type="text"]').val('');
+            $('#EventInputForm input[type="date"]').val('');
+            $('#EventInputForm input[type="time"]').val('');
+            $('#EventInputForm #eventAddress').val('');
+            $('#EventInputForm #eventDescription').val('');
+            $scope.ResetEventModel();
+        }
+    };
+}]);
